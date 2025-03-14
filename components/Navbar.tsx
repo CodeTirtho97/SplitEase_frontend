420c6d798017f8e033745ccee75c5a1ed05938fb
Tirthoraj Bhattacharya <tirthorajbhattacharya@gmail.com>
2025-03-14 19:54:58 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -174,7 +174,8 @@ export default function Navbar() {
               <Button
                 text="Sign Out"
                 onClick={handleLogout}
-                className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition shadow-md"
+                variant="danger"
+                size="lg"
               />
             </>
           ) : (
